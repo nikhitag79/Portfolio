8b27a71f7ebcd255d97f4ba72b2b7ee656c1e80c
nikhitag79 <nguntu@gmail.com>
2024-01-09 19:32:01 -0500

    adding sideBar

--- Website/react-portfolio/src/App.js
+++ Website/react-portfolio/src/App.js
@@ -3,11 +3,13 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout'
 
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
       {/* More routes can be added here */}
+      {/* <Route index element={<Home />} /> */}
   </Routes>
   );
 }
--- Website/react-portfolio/src/components/Sidebar/index.js
+++ Website/react-portfolio/src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub, faYoutube, faSkype} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -15,7 +16,7 @@ const Sidebar = () => (
             <NavLink exact ="true" activeclassname="active" to ="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact ="true" activeclassname="active" className = "about-link" to ="/">
+            <NavLink exact ="true" activeclassname="active" className = "about-link" to ="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
             <NavLink exact ="true" activeclassname="active" className = "contact-link" to ="/contact">
@@ -23,6 +24,20 @@ const Sidebar = () => (
             </NavLink>
 
         </nav>
+        <ul>
+            <li>
+                <a target = "_blank" rel = "noreferrer" href= "https://www.linkedin.com/in/nikhita-guntu-186807188/" >
+                    <FontAwesomeIcon icon = {faLinkedin} color = '#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target = "_blank" rel = "noreferrer" href= "https://github.com/" >
+                    <FontAwesomeIcon icon = {faGithub} color = '#4d4d4e' />
+                </a>
+            </li>
+
+
+        </ul>
 
     </div>
 
