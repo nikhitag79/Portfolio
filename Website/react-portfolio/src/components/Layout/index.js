74f90fd9ebf45a3a2b67fbef8270665ccd05303e
nikhitag79 <nguntu@gmail.com>
2024-01-23 22:53:32 -0500

    changed UI tag

--- Website/react-portfolio/src/components/Layout/index.js
+++ Website/react-portfolio/src/components/Layout/index.js
@@ -12,7 +12,7 @@ const Layout = () => {
 
             <span className = 'tags bottom-tags'>&lt;/body&gt; 
             <br />
-            <span className ='bottom-tag-html'>&lt;/html&gt</span>
+            <span className ='bottom-tag-html'>&lt;/html&gt;</span>
             </span>
             </div>
         </div>
